65e397dc318fdcef9496c16246e82e0bdfa81598
martin <martincharles07@gmail.com>
2024-06-22 14:26:29 -0500

    fix hex parsing
    javascript has a poorly specified url parser

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-type BencodableValue = number | string | BencodableValue[] | { [key: string]: BencodableValue };
-
-function bencode(data: BencodableValue): string {
-	if (typeof data === 'number') {
-		return `i${data}e`;
-	} else if (typeof data === 'string') {
-		return `${data.length}:${data}`;
-	} else if (Array.isArray(data)) {
-		return `l${data.map(bencode).join('')}e`;
-	} else if (typeof data === 'object') {
-		const encoded = Object.keys(data)
-			.sort()
-			.map((key) => `${bencode(key)}${bencode(data[key])}`)
-			.join('');
-		return `d${encoded}e`;
-	} else {
-		throw new Error('invalid data type');
-	}
-}
+import * as p from './parse';
 
 type PeerStateArgs = {
 	ip: string;
@@ -43,38 +25,67 @@ export class TorrentState implements DurableObject {
 		const url = new URL(request.url);
 		const params = url.searchParams;
 
-		const peerId = params.get('peer_id');
-		const port = parseInt(params.get('port')!!);
-		const uploaded = parseInt(params.get('uploaded')!!);
-		const downloaded = parseInt(params.get('downloaded')!!);
-		const left = parseInt(params.get('left')!!);
-		const event = params.get('event')!!;
+		if (url.pathname == '/') {
+			const peerId = params.get('peer_id');
+			if (!peerId) {
+				return new Response(
+					bencode({
+						'failure code': 102,
+					}),
+				);
+			}
 
-		if (!peerId || isNaN(port)) {
-			return new Response('Invalid request', { status: 400 });
+			if (peerId.length !== 20) {
+				return new Response(
+					bencode({
+						'failure code': 151,
+					}),
+				);
+			}
+
+			const port = tryExtractNumber(params.get('port'));
+			if (!port) {
+				return new Response(
+					bencode({
+						'failure code': 103,
+					}),
+				);
+			}
+
+			const uploaded = tryExtractNumber(params.get('uploaded')) ?? 0;
+			const downloaded = tryExtractNumber(params.get('downloaded')) ?? 0;
+			const left = tryExtractNumber(params.get('left')) ?? 0;
+			const event = params.get('event');
+
+			const ip = params.get('ip') ?? request.headers.get('cf-connecting-ip')!;
+
+			await this.updatePeer(peerId, { ip, port, uploaded, downloaded, left }, event);
+
+			await this.cleanupPeers();
+
+			const peerList = await this.getPeerList();
+
+			const response = {
+				interval: 1800,
+				peers: peerList,
+			};
+
+			const encodedResponse = bencode(response);
+
+			return new Response(encodedResponse, {
+				headers: { 'Content-Type': 'text/plain' },
+			});
+		} else {
+			return new Response(JSON.stringify(await this.getPeerList()));
 		}
-
-		const ip = request.headers.get('cf-connecting-ip')!;
-
-		await this.updatePeer(peerId, { ip, port, uploaded, downloaded, left });
-
-		await this.cleanupPeers();
-
-		const peerList = await this.getPeerList();
-
-		const response = {
-			interval: 1800,
-			peers: peerList,
-		};
-
-		const encodedResponse = bencode(response);
-
-		return new Response(encodedResponse, {
-			headers: { 'Content-Type': 'text/plain' },
-		});
 	}
 
-	async updatePeer(peerId: string, data: PeerStateArgs) {
+	async updatePeer(peerId: string, data: PeerStateArgs, event: string | null) {
+		if (event === 'stopped') {
+			await this.storage.delete(peerId);
+			return;
+		}
+
 		await this.storage.put<PeerStateStored>(peerId, {
 			...data,
 			lastAnnounce: Date.now(),
@@ -101,17 +112,122 @@ export class TorrentState implements DurableObject {
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
-		const infoHash = url.searchParams.get('info_hash');
+		const params = parseQueryString(url.search.slice(1));
 
-		if (!infoHash) {
-			return new Response('Invalid request', { status: 400 });
+		if (url.pathname === '/') {
+			const encodedInfoHash = params.get('info_hash');
+
+			if (!encodedInfoHash) {
+				return new Response(
+					bencode({
+						'failure code': 101,
+					}),
+				);
+			}
+
+			const infoHashDecoded = parseQueryStringValue(encodedInfoHash);
+			if (infoHashDecoded.length !== 20) {
+				return new Response(
+					bencode({
+						'failure code': 150,
+					}),
+				);
+			}
+
+			const infoHash = intoHex(infoHashDecoded);
+
+			const id = env.TORRENT_STATE.idFromName(infoHash);
+			const tracker = env.TORRENT_STATE.get(id);
+
+			return tracker.fetch(request);
+		} else {
+			const infoHash = url.pathname.slice(1);
+			if (infoHash.length != 40) {
+				return new Response('invalid info hash', { status: 400 });
+			}
+
+			const id = env.TORRENT_STATE.idFromName(infoHash.toLowerCase());
+			const tracker = env.TORRENT_STATE.get(id);
+
+			return tracker.fetch(request);
 		}
-
-		// Get Durable Object stub
-		const id = env.TORRENT_STATE.idFromName(infoHash);
-		const tracker = env.TORRENT_STATE.get(id);
-
-		// Forward request to Durable Object
-		return tracker.fetch(request);
 	},
 } satisfies ExportedHandler<Env>;
+
+export function intoHex(array: Uint8Array) {
+	return [...array].map((b) => b.toString(16).padStart(2, '0')).join('');
+}
+
+export function parseQueryStringValue(queryString: string): Uint8Array {
+	const result = p.parseRepeated(
+		p.alt(
+			p.map(
+				p.sequence(
+					p.tag('%'),
+					p.takeExactly(
+						2,
+						p.parseMatching((input) => /[0-9a-fA-F]/.test(input)),
+					),
+				),
+				([_, char]) => parseInt(char.join(''), 16),
+			),
+			p.map(
+				p.parseMatching(
+					(char) => !['!', '*', "'", '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '#', '[', ']'].includes(char),
+				),
+				(char) => char.charCodeAt(0),
+			),
+		),
+	)(queryString);
+
+	if (!result || result.remaining.length > 0) {
+		throw new Error('invalid query string');
+	}
+
+	return new Uint8Array(result.value);
+}
+
+export function parseQueryString(queryString: string): Map<string, string> {
+	const params = new Map<string, string>();
+	const pairs = queryString.split('&');
+	for (const pair of pairs) {
+		const [key, value] = pair.split('=');
+		if (key && value) {
+			params.set(decodeURIComponent(key), value);
+		}
+	}
+	return params;
+}
+
+function tryExtractNumber(value: string | null): number | null {
+	if (!value) {
+		return null;
+	}
+
+	const parsedValue = parseInt(value);
+	if (!(parsedValue >= 0)) {
+		return null;
+	}
+
+	return parsedValue;
+}
+
+type BencodableValue = number | string | BencodableValue[] | { [key: string]: BencodableValue };
+
+function bencode(data: BencodableValue): string {
+	if (typeof data === 'number') {
+		return `i${data}e`;
+	} else if (typeof data === 'string') {
+		return `${data.length}:${data}`;
+	} else if (Array.isArray(data)) {
+		return `l${data.map(bencode).join('')}e`;
+	} else if (typeof data === 'object') {
+		const encoded = Object.keys(data)
+			.sort()
+			.map((key) => `${bencode(key)}${bencode(data[key])}`)
+			.join('');
+		return `d${encoded}e`;
+	} else {
+		throw new Error('invalid data type');
+	}
+}
---
+++ src/parse.ts
@@ -0,0 +1,130 @@
+export type ParseResult<T> = {
+	remaining: string;
+	value: T;
+};
+
+export type Parser<T> = (input: string) => ParseResult<T> | null;
+
+export function map<T1, T2>(parser: Parser<T1>, fn: (value: T1) => T2): Parser<T2> {
+	return (input) => {
+		const result = parser(input);
+		if (!result) {
+			return null;
+		}
+
+		return {
+			remaining: result.remaining,
+			value: fn(result.value),
+		};
+	};
+}
+
+export function sequence<T1, T2>(parser1: Parser<T1>, parser2: Parser<T2>): Parser<[T1, T2]> {
+	return (input) => {
+		const result1 = parser1(input);
+		if (!result1) {
+			return null;
+		}
+
+		const result2 = parser2(result1.remaining);
+		if (!result2) {
+			return null;
+		}
+
+		return {
+			remaining: result2.remaining,
+			value: [result1.value, result2.value],
+		};
+	};
+}
+
+export function alt<T>(parser1: Parser<T>, parser2: Parser<T>): Parser<T> {
+	return (input) => {
+		const result1 = parser1(input);
+		if (result1) {
+			return result1;
+		}
+
+		const result2 = parser2(input);
+		if (result2) {
+			return result2;
+		}
+
+		return null;
+	};
+}
+
+export function tag(value: string): Parser<string> {
+	return (input) => {
+		if (input.startsWith(value)) {
+			return {
+				remaining: input.slice(value.length),
+				value,
+			};
+		} else {
+			return null;
+		}
+	};
+}
+
+export function takeExactly<Inner>(count: number, parse: Parser<Inner>): Parser<Inner[]> {
+	return (input) => {
+		const results: Inner[] = [];
+		let remaining = input;
+
+		for (let i = 0; i < count; i++) {
+			const result = parse(remaining);
+			if (!result) {
+				return null;
+			}
+
+			results.push(result.value);
+			remaining = result.remaining;
+		}
+
+		return {
+			remaining,
+			value: results,
+		};
+	};
+}
+
+export function parseRepeated<Inner>(parseFn: Parser<Inner>): Parser<Inner[]> {
+	return (input) => {
+		const results: Inner[] = [];
+		let remaining = input;
+
+		while (true) {
+			const result = parseFn(remaining);
+			if (!result) {
+				break;
+			}
+
+			results.push(result.value);
+			remaining = result.remaining;
+		}
+
+		return {
+			remaining,
+			value: results,
+		};
+	};
+}
+
+export function parseMatching(predicate: (char: string) => boolean): Parser<string> {
+	return (input) => {
+		if (input.length < 1) {
+			return null;
+		}
+
+		const char = input[0];
+		if (!predicate(char)) {
+			return null;
+		}
+
+		return {
+			remaining: input.slice(1),
+			value: char,
+		};
+	};
+}
