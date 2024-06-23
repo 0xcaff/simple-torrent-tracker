f42012f307518909ca348f9474a7bae1e0ebc560
martin <martincharles07@gmail.com>
2024-06-22 22:30:51 -0500

    use rpc
    didn't realize this was possible
    
    also lock down info hashes, don't want to get captured

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import { DurableObject } from "cloudflare:workers";
+
 import * as p from "./parse";
+
+interface Env {
+  TORRENT_STATE: DurableObjectNamespace<TorrentState>;
+  DD_API_KEY?: string;
+  ALLOWED_INFO_HASHES?: string;
+}
 
 type PeerStateArgs = {
   ip: string;
@@ -12,102 +20,49 @@ type PeerStateStored = PeerStateArgs & {
   lastAnnounce: number;
 };
 
-export class TorrentState implements DurableObject {
-  private storage: DurableObjectStorage;
-  private state: DurableObjectState;
-  private logger: DatadogLogger;
+type AnnounceArguments = {
+  peerId: string;
+  ip: string;
+  port: number;
+  event: string | null;
+  uploaded: number;
+  downloaded: number;
+  left: number;
+};
 
-  constructor(state: DurableObjectState, env: Env) {
-    this.storage = state.storage;
-    this.state = state;
-    this.logger = new DatadogLogger(env.DD_API_KEY);
-  }
+export class TorrentState extends DurableObject {
+  async handleAnnounce({
+    peerId,
+    ip,
+    port,
+    event,
+    uploaded,
+    downloaded,
+    left,
+  }: AnnounceArguments) {
+    await this.updatePeer(
+      peerId,
+      { ip, port, uploaded, downloaded, left },
+      event,
+    );
 
-  async fetch(request: Request) {
-    const url = new URL(request.url);
-    const params = url.searchParams;
+    await this.cleanupPeers();
 
-    if (url.pathname == "/") {
-      const peerId = params.get("peer_id");
-      if (!peerId) {
-        return new Response(
-          bencode({
-            "failure code": 102,
-          }),
-        );
-      }
+    const peerList = await this.getPeerList();
 
-      if (peerId.length !== 20) {
-        return new Response(
-          bencode({
-            "failure code": 151,
-          }),
-        );
-      }
-
-      const port = tryExtractNumber(params.get("port"));
-      if (!port) {
-        return new Response(
-          bencode({
-            "failure code": 103,
-          }),
-        );
-      }
-
-      const uploaded = tryExtractNumber(params.get("uploaded")) ?? 0;
-      const downloaded = tryExtractNumber(params.get("downloaded")) ?? 0;
-      const left = tryExtractNumber(params.get("left")) ?? 0;
-      const event = params.get("event");
-
-      const ip = params.get("ip") ?? request.headers.get("cf-connecting-ip")!;
-
-      await this.updatePeer(
-        peerId,
-        { ip, port, uploaded, downloaded, left },
-        event,
-      );
-
-      await this.cleanupPeers();
-
-      this.state.waitUntil(
-        this.logger.log([
-          {
-            type: "announce",
-            values: {
-              ip,
-              port,
-              uploaded,
-              downloaded,
-              left,
-            }
-          },
-        ]),
-      );
-
-      const peerList = await this.getPeerList();
-
-      const response = {
-        interval: 1800,
-        peers: peerList,
-      };
-
-      const encodedResponse = bencode(response);
-
-      return new Response(encodedResponse, {
-        headers: { "Content-Type": "text/plain" },
-      });
-    } else {
-      return new Response(JSON.stringify(await this.getPeerList()));
-    }
+    return bencode({
+      interval: 1800,
+      peers: peerList,
+    });
   }
 
   async updatePeer(peerId: string, data: PeerStateArgs, event: string | null) {
     if (event === "stopped") {
-      await this.storage.delete(peerId);
+      await this.ctx.storage.delete(peerId);
       return;
     }
 
-    await this.storage.put<PeerStateStored>(peerId, {
+    await this.ctx.storage.put<PeerStateStored>(peerId, {
       ...data,
       lastAnnounce: Date.now(),
     });
@@ -115,15 +70,15 @@ export class TorrentState implements DurableObject {
 
   async cleanupPeers() {
     const now = Date.now();
-    for (const [key, peer] of await this.storage.list<PeerStateStored>()) {
+    for (const [key, peer] of await this.ctx.storage.list<PeerStateStored>()) {
       if (now - peer.lastAnnounce > 30 * 60 * 1000) {
-        await this.storage.delete(key);
+        await this.ctx.storage.delete(key);
       }
     }
   }
 
   async getPeerList(): Promise<PeerStateStored[]> {
-    const stored = await this.storage.list<PeerStateStored>({
+    const stored = await this.ctx.storage.list<PeerStateStored>({
       limit: 50,
     });
     return Array.from(stored.values());
@@ -133,9 +88,19 @@ export class TorrentState implements DurableObject {
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    const params = parseQueryString(url.search.slice(1));
+
+    function isInAllowedInfoHashes(infoHash: string) {
+      if (!env.ALLOWED_INFO_HASHES) {
+        // Allow all by default
+        return true;
+      }
+
+      return env.ALLOWED_INFO_HASHES.split(",").includes(infoHash);
+    }
 
     if (url.pathname === "/") {
+      const params = parseQueryString(url.search.slice(1));
+
       const encodedInfoHash = params.get("info_hash");
 
       if (!encodedInfoHash) {
@@ -156,21 +121,95 @@ export default {
       }
 
       const infoHash = intoHex(infoHashDecoded);
+      if (!isInAllowedInfoHashes(infoHash)) {
+        return new Response(
+          bencode({
+            "failure code": 200,
+          }),
+        );
+      }
+
+      const peerId = params.get("peer_id");
+      if (!peerId) {
+        return new Response(
+          bencode({
+            "failure code": 102,
+          }),
+        );
+      }
+
+      if (peerId.length !== 20) {
+        return new Response(
+          bencode({
+            "failure code": 151,
+          }),
+        );
+      }
+
+      const port = tryExtractNumber(params.get("port") ?? null);
+      if (!port) {
+        return new Response(
+          bencode({
+            "failure code": 103,
+          }),
+        );
+      }
+
+      const uploaded = tryExtractNumber(params.get("uploaded") ?? null) ?? 0;
+      const downloaded =
+        tryExtractNumber(params.get("downloaded") ?? null) ?? 0;
+      const left = tryExtractNumber(params.get("left") ?? null) ?? 0;
+      const event = params.get("event") ?? null;
+
+      const ip = params.get("ip") ?? request.headers.get("cf-connecting-ip")!;
+
+      if (env.DD_API_KEY) {
+        const logger = new DatadogLogger(env.DD_API_KEY);
+        ctx.waitUntil(
+          logger.log([
+            {
+              type: "announce",
+              values: {
+                infoHash,
+                ip,
+                port,
+                uploaded,
+                downloaded,
+                left,
+              },
+            },
+          ]),
+        );
+      }
 
       const id = env.TORRENT_STATE.idFromName(infoHash);
       const tracker = env.TORRENT_STATE.get(id);
 
-      return tracker.fetch(request);
+      const response = await tracker.handleAnnounce({
+        peerId,
+        ip,
+        port,
+        uploaded,
+        downloaded,
+        left,
+        event,
+      });
+
+      return new Response(response);
     } else {
       const infoHash = url.pathname.slice(1);
       if (infoHash.length != 40) {
-        return new Response("invalid info hash", { status: 400 });
+        return new Response("not found", { status: 404 });
+      }
+
+      if (!isInAllowedInfoHashes(infoHash)) {
+        return new Response("not found", { status: 404 });
       }
 
       const id = env.TORRENT_STATE.idFromName(infoHash.toLowerCase());
       const tracker = env.TORRENT_STATE.get(id);
 
-      return tracker.fetch(request);
+      return new Response(JSON.stringify(await tracker.getPeerList()));
     }
   },
 } satisfies ExportedHandler<Env>;
