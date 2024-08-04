f613aa909d50675e39fc38f2705fc033c7beb947
martin <martincharles07@gmail.com>
2024-08-04 16:10:33 -0500

    add additional test

--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -1,6 +1,10 @@
 import { it, expect } from "vitest";
-import { parseQueryString, parseQueryStringValue } from '../src/parseQueryString/parseQueryString';
-import { intoHex } from '../src/bencode';
+import {
+  parseQueryString,
+  parseQueryStringValue,
+} from "../src/parseQueryString/parseQueryString";
+import { intoHex } from "../src/bencode";
+import { parseAnnounceRequestArgs } from "../src/parseAnnounceRequestArgs";
 
 it("parses a query string into its constituent parts", () => {
   expect(
@@ -33,4 +37,28 @@ it("parses a query string value", () => {
   expect(encoded).toMatchInlineSnapshot(
     `"cabf1b0337adfb62bc0b50f15bce770e84791763"`,
   );
+});
+
+it("extracts", () => {
+  expect(
+    parseAnnounceRequestArgs(
+      parseQueryString(
+        "info_hash=%E1%B8m%C6%5E%1A%11%DE%16%90%1F%26%98%82%80r%40%C1%7Fr&peer_id=-TR4050-x4apaio8s299&port=51413&uploaded=0&downloaded=0&left=278672696&numwant=80&key=0A850B43&compact=1&supportcrypto=1&event=started",
+      ),
+    ),
+  ).toMatchInlineSnapshot(`
+    {
+      "type": "SUCCESS",
+      "values": {
+        "downloaded": 0,
+        "event": "started",
+        "infoHash": "e1b86dc65e1a11de16901f269882807240c17f72",
+        "ip": null,
+        "left": 278672696,
+        "peerId": "-TR4050-x4apaio8s299",
+        "port": 51413,
+        "uploaded": 0,
+      },
+    }
+  `);
 });
